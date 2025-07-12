8b50a2871e4ef72424449953d61a6cf281bb89fd
Vanessa Mineiro da Silva <37229001+vanessamineiro@users.noreply.github.com>
2025-07-12 15:51:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-//AIzaSyALT-lFpSVEewqSnbtkotiF2NqCROHgUmY
 const perguntarAI = async (question, game, apiKey) => {
   const model = "gemini-2.0-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
